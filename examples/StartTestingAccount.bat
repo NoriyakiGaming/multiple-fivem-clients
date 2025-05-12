0e01e2348387cca7a40da9ff7f4433f6edfdf242
NoriyakiGaming <91399559+NoriyakiGaming@users.noreply.github.com>
2025-05-12 13:29:04 +0200

    Update StartTestingAccount.bat

--- examples/StartTestingAccount.bat
+++ examples/StartTestingAccount.bat
@@ -1,4 +1,3 @@
-copy "%userprofile%\AppData\Local\DigitalEntitlements\AccountsData\38d8f400-aa8a-4784-a9f0-26a08628577e_TestAccount" "%userprofile%\AppData\Local\DigitalEntitlements\38d8f400-aa8a-4784-a9f0-26a08628577e"
 start FiveM.exe -cl2 -windowed
 
 exit
